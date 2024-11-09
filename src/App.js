6e1b1aa2a231fb88a06820b1471a264e11597921
sanghee01 <sang6299@naver.com>
2024-11-09 15:57:09 +0900

    feat: 제품, 프로모션 파일 불러오기 및 변수 세팅
    products.md, promotion.md 파일에 있는 정보를 가져와 각각 Map 생성자를 통해 리스트를 초기화해 각 배열에 저장함

--- src/App.js
+++ src/App.js
@@ -1,5 +1,59 @@
+import { Console } from '@woowacourse/mission-utils';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 class App {
-  async run() {}
+  async run() {
+    // 파일 불러오기
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const productsFilePath = path.join(__dirname, '../public/products.md');
+    const promotionsFilePath = path.join(__dirname, '../public/promotions.md');
+    const products = fs
+      .readFileSync(productsFilePath, 'utf-8')
+      .trim()
+      .split('\n');
+    const promotions = fs
+      .readFileSync(promotionsFilePath, 'utf-8')
+      .trim()
+      .split('\n');
+
+    // 제품 목록 세팅
+    const productsList = [];
+    const [PRODUCT_NAME, PRICE, QUANTITY, PROMOTION] = products
+      .shift()
+      .split(',');
+    products.forEach((product) => {
+      const productInfo = product.split(',');
+      const productMap = new Map();
+      productMap.set(PRODUCT_NAME, productInfo[0]);
+      productMap.set(PRICE, Number(productInfo[1]));
+      productMap.set(QUANTITY, Number(productInfo[2]));
+      productMap.set(PROMOTION, productInfo[3]);
+      if (productInfo[3] === 'null') {
+        productMap.set(PROMOTION, '');
+      }
+
+      productsList.push(productMap);
+    });
+
+    // 프로모션 목록 세팅
+    const promotionsList = [];
+    const [PROMOTION_NAME, BUT, GET, START_DATE, END_DATE] = promotions
+      .shift()
+      .split(',');
+    promotions.forEach((promotion) => {
+      const promotionInfo = promotion.split(',');
+      const promotionMap = new Map();
+      promotionMap.set(PROMOTION_NAME, promotionInfo[0]);
+      promotionMap.set(BUT, Number(promotionInfo[1]));
+      promotionMap.set(GET, Number(promotionInfo[2]));
+      promotionMap.set(START_DATE, promotionInfo[3]);
+      promotionMap.set(END_DATE, promotionInfo[4]);
+      promotionsList.push(promotionMap);
+    });
+  }
 }
 
 export default App;
